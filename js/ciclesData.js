aa52daf341aed44ae3e761b4d0a695df2fdfde9c
Lorena Sánchez Sánchez <l.sanchezsanchez2@edu.gva.es>
2026-05-11 11:47:25 +0200

    Update ciclesData.js

--- js/ciclesData.js
+++ js/ciclesData.js
@@ -459,7 +459,7 @@ accent: "#a0c550",
 ],continu: [
   '<div class="panel-section">',
   '<h3>🚀 Continuïtat formativa</h3>',
-  "<p>Altres cursos d'especialització:</p>"
+  "<p>Altres cursos d'especialització:</p>",
   '<ul>',
   '<li>Ciberseguretat</li>',
   '<li>Desenvolupament de videojocs</li>',
